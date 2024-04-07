7a0474cc03fafe3aa82a781693bcd70fdac80a93
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-04-07 18:08:34 +0200

    V.1

--- app/api/testimonials/getTestimonials.jsx
+++ app/api/testimonials/getTestimonials.jsx
@@ -3,9 +3,9 @@ import { createClient } from "contentful";
 import { useEffect, useState } from "react";
 
 const client = createClient({
-  space: process.env.SPACE_ID,
+  space: process.env.NEXT_PUBLIC_SPACE_ID,
   environment: "master",
-  accessToken: process.env.CONTENTFUL_TOKEN,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_TOKEN,
 });
 export const useGetTestimonials = () => {
   const [loading, setLoading] = useState(true);
